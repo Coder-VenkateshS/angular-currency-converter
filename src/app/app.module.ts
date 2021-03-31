6d820c7501061de756d6e544dd16feacf6a47e7a
Teacher-Venkatesh <12venkat21@gmail.com>
2021-03-31 18:26:40 +0530

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { ConvertPipe } from "./convert.pipe";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ConverterPipe } from "./converter.pipe";
 
 @NgModule({
   declarations: [AppComponent, ConverterPipe, ConvertPipe],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
